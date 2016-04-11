cdd6b6471a24d0af8080b2d32d4513fd28458dfb
Fohlen <lennard@berger.work>
2016-04-11 13:16:13 +0200

    Added very basic bot functionality with 2 commands and WebHooks

---
+++ app/Bot.php
@@ -0,0 +1,60 @@
+<?php
+
+class Bot extends \Prefab {
+	private $_BASE_URL; // Telegram API URL
+	// A list of commands
+	const COMMANDS = array(
+		'mix',
+		'cw'
+	);
+	
+	/**
+	 * @return string[0] command, string[1] argument
+	 * @throws Error for no/unknown command
+	 * @param string $string
+	 */
+	private function _parseCommand($string) {
+		if (strpos($string, '/') != 0 ) throw new Exception('No command found');
+		$commands = explode(' ', $string, 2);
+		if (in_array($commands[0], COMMANDS))
+			return $commands;
+		else
+			throw new Exception('Command not found');
+	}
+	
+	public function __construct() {
+		// Initialise the base url for Telegram
+		$this->_BASE_URL = 'https://api.telegram.org/bot' . \Base::instance()->get('bot.token');
+	}
+	
+	public function respondMessage($message) {
+		$command = $this->_parseCommand($message->text);
+		$options = array(
+				'method' => 'POST',
+				'Content-Type' => 'application/json',
+		);
+		
+		switch($command[0]) {
+			case 'mix':
+				$response = $message->from->first_name . ' requested a mix at ' . $command[1];
+				$responseMessage = array(
+					'chat_id' => $message->chat->id,
+					'text' => $response
+				);
+				$options['content'] = json_encode($responseMessage);
+				
+				$result = \Web::instance()->request($this->_BASE_URL . '/sendMessage', $options);
+				break;
+			case 'cw':
+				$response = $message->from->first_name . ' is looking for a clanwar. ' . $command[1];
+				$responseMessage = array(
+						'chat_id' => $message->chat->id,
+						'text' => $response
+				);
+				$options['content'] = json_encode($responseMessage);
+				
+				$result = \Web::instance()->request($this->_BASE_URL . '/sendMessage', $options);
+				break;
+		}
+	}
+}
--- index.php
+++ index.php
@@ -3,8 +3,18 @@
 require_once('lib/autoload.php');
 
 $f3 = Base::instance();
+$web = Web::instance();
 
-//$f3->config('app/config/configs.ini'); // This will pull all the necessary configuration files
+$f3->config('app/config.ini'); // This will pull all the necessary configuration files
+
+$f3->route('POST /' . $f3->get('bot.token'), function($f3) {
+	// The update ID is not yet handled since not yet needed
+	// Will be for spam protection
+	if (isset($f3->get('POST.message'))) {
+		$message = $f3->get('POST.message');
+		Bot::instance()->respondMessage($message);
+	}
+});
 
 // Fire it up
 $f3->run();
